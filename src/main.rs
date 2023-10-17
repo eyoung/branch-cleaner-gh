dbe7feff04618195ee630b8d271dd273eb9da746
Eric Young <soft.luck1107@madsciencesoftware.com>
2023-10-17 07:54:59 -0600

    Initial exploration test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod test {
+    use std::env;
+
+    use git2::{Repository, Branches};
+
+
+    #[test]
+    fn can_list_local_branches_in_repository() {
+        let path = env::current_dir().unwrap();
+        let repo = Repository::open(path).unwrap();
+        
+        let branches: Branches<'_> = repo.branches(Some(git2::BranchType::Local)).unwrap();
+
+        let expected = vec!["main".to_owned()];
+        let branch_names: Vec<String> = branches.map(|b| {
+            let n = b.unwrap().0.name().unwrap().unwrap().to_owned();
+            n
+        }).collect();
+
+        println!("{:?}", branch_names);
+
+        assert_eq!(expected, branch_names)
+    }
+}
